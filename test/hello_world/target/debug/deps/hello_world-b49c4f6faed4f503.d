6828bdc934ed7729aa2335bd68b9290df4c48c03
AmadeusWM <63149896+AmadeusWM@users.noreply.github.com>
2024-11-24 18:44:31 +0100

    remove target

--- test/hello_world/target/debug/deps/hello_world-b49c4f6faed4f503.d
+++
@@ -1,5 +0,0 @@
-/home/amadeusw/university/master1/compilers/mini-rust-compiler/test/hello_world/target/debug/deps/hello_world-b49c4f6faed4f503: src/main.rs
-
-/home/amadeusw/university/master1/compilers/mini-rust-compiler/test/hello_world/target/debug/deps/hello_world-b49c4f6faed4f503.d: src/main.rs
-
-src/main.rs:
--- test/hello_world/target/debug/deps/hello_world-ca6f3ed1ca05e05e.d
+++
@@ -1,5 +0,0 @@
-/home/amadeusw/university/master1/compilers/mini-rust-compiler/test/hello_world/target/debug/deps/hello_world-ca6f3ed1ca05e05e: src/main.rs
-
-/home/amadeusw/university/master1/compilers/mini-rust-compiler/test/hello_world/target/debug/deps/hello_world-ca6f3ed1ca05e05e.d: src/main.rs
-
-src/main.rs:
